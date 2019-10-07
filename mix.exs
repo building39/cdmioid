dbae598b23a9ae2282fa106f567ac48a186a2526
mmartin <mmartin4242@gmail.com>
2019-10-07 18:13:36 -0400

    bump version number

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Cdmioid.Mixfile do
 
   def project do
     [app: :cdmioid,
-     version: "0.1.0",
+     version: "0.1.1",
      elixir: "~> 1.4-dev",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
