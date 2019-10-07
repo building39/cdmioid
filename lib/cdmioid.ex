21e6e82af29d5abe7987ad53dca06d2c605ae14e
mmartin <mmartin4242@gmail.com>
2019-10-07 18:12:50 -0400

    Don't try to flip the key.

--- lib/cdmioid.ex
+++ lib/cdmioid.ex
@@ -4,13 +4,11 @@ defmodule Cdmioid do
   """
 
   def generate(enterprise_number, oid_length \\ 40) do
-    prefix = <<0, enterprise_number :: size(24), 0, oid_length>>
+    prefix = <<0, enterprise_number::size(24), 0, oid_length>>
     uuid = UUID.uuid4(:hex)
     crc = Elcrc16.crc16(prefix <> <<0, 0>> <> uuid)
-    new_prefix = Base.encode16(prefix <> <<crc :: size(16)>>) |> String.downcase
-    key = uuid <> new_prefix
+    new_prefix = Base.encode16(prefix <> <<crc::size(16)>>) |> String.downcase()
     oid = new_prefix <> uuid
-    {oid, key}
+    oid
   end
-
 end
